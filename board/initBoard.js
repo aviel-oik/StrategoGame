67af540dbaae7f897d30da17cd1d2089df47bc9e
Aviel Oiknine <avieloikpro@gmail.com>
2025-12-08 10:48:05 +0200

    Refactor soldier creation and movement logic; integrate soldier placement into game flow

--- board/initBoard.js
+++ board/initBoard.js
@@ -17,34 +17,34 @@ function creatSoldier(rank, owner) {
     }
 }
 
-function createSoldierList() {
+function createSoldierList(owner) {
     let SoldiersLst = []
     for (let i = 1; i <= 11; i++) {
         if (i === 1 || i === 9 || i === 10 || i === 11)
-            SoldiersLst.push(creatSoldier(i))
+            SoldiersLst.push(creatSoldier(i, owner))
         if (i === 8)
             for (let i = 0; i < 2; i++)
-                SoldiersLst.push(creatSoldier(i))
+                SoldiersLst.push(creatSoldier(i, owner))
         if (i === 7)
             for (let i = 0; i < 3; i++)
-                SoldiersLst.push(creatSoldier(i))
+                SoldiersLst.push(creatSoldier(i, owner))
         if (i === 4 || i === 5 || i === 6)
             for (let i = 0; i < 4; i++)
-                SoldiersLst.push(creatSoldier(i))
+                SoldiersLst.push(creatSoldier(i, owner))
         if (i === 3)
             for (let i = 0; i < 5; i++)
-                SoldiersLst.push(creatSoldier(i))
+                SoldiersLst.push(creatSoldier(i, owner))
         if (i === 2)
             for (let i = 0; i < 8; i++)
-                SoldiersLst.push(creatSoldier(i))
+                SoldiersLst.push(creatSoldier(i, owner))
     }
     return SoldiersLst
 }
 
 export function placeSoldier() {
     let board = createBoard(10)
-    let computerSoldiersLst = createSoldierList()
-    let playerSoldiersLst = createSoldierList()
+    let computerSoldiersLst = createSoldierList("computer")
+    let playerSoldiersLst = createSoldierList("player")
     for (let i = 0; i < 3; i++) { // need to calculate 
         for (let j = 0; j < board.length; j++) {
             const computerSoldier = computerSoldiersLst.splice(Math.random() * computerSoldiersLst.length)
--- gameLogic/game.js
+++ gameLogic/game.js
@@ -1,8 +1,70 @@
-function moveSoldier() {
+import { placeSoldier } from "../board/initBoard"
+import readline from "readline-sync"
 
+function movement(x, y, board) {
+    if (board[x][y] == 0) {
+        console.log("no soldier in this position, please choose another one")
+        return false
+    }
+    if (board[x][y].owner !== "player") {
+        console.log("this is not your soldier, please choose another one")
+        return false
+    }
+    const possibilites = moveSoldierPossibilities(x, y, board)
+    console.log("you can move your soldier to the following directions: " + possibilites)
+    let direction = readline.question("please choose a direction to move your soldier: ")
+    if (!possibilites.includes(direction)) {
+        console.log("invalid direction, please choose another one")
+        return false
+    }
+    moveSoldier(x, y, direction, board)
 }
 
-function fight() {
+function moveSoldierPossibilities(x, y, board) {
+    possibilites = []
+    if (y > 0 && board[x][y - 1].owner !== "player")
+        possibilites.push("backward")
+    if (y < board.length - 1 && board[x][y + 1].owner !== "player")
+        possibilites.push("forward")
+    if (x > 0 && board[x - 1][y].owner !== "player")
+        possibilites.push("left")
+    if (x < board.length - 1 && board[x + 1][y].owner !== "player")
+        possibilites.push("right")
+    return possibilites
+}
+
+function moveSoldier(x, y, direction, board) {
+    let soldier = board[x][y]
+    board[x][y] = 0
+    if (direction === "backward")
+        if (board[x][y - 1] == 0)
+            board[x][y - 1] = soldier
+        else
+            defender = board[x][y - 1]
+    fight(x, y, defender, board)
+    if (direction === "forward")
+        if (board[x][y + 1] == 0)
+            board[x][y + 1] = soldier
+        else
+            defender = board[x][y + 1]
+    fight(x, y, defender, board)
+    if (direction === "left")
+        if (board[x - 1][y] == 0)
+            board[x - 1][y] = soldier
+        else
+            defender = board[x - 1][y]
+    fight(x, y, defender, board)
+    if (direction === "right")
+        if (board[x + 1][y] == 0)
+            board[x + 1][y] = soldier
+        else
+            defender = board[x + 1][y]
+    fight(x, y, defender, board)
+}
+
+function fight(x, y, defender, board) {
+    let attacker = board[x][y]
+
 
 }
 
@@ -11,5 +73,11 @@ export function Victory() {
 }
 
 export function playRound() {
-
+    board = placeSoldier
+    console.log("Youn turn to play")
+    soldierToMove = readline.question("please choose a soldier to move (x,y)")
+    x = parseInt(soldierToMove[1])
+    y = parseInt(soldierToMove[3])
+    endGame = movement(x, y, board)
+    return endGame
 }
