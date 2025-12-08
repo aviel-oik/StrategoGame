25b8504fda328387cc89965bf7ac7477f430ec77
Your Name <you@example.com>
2025-12-08 12:10:54 +0200

    Implement board printing functionality and integrate soldier placement

--- board/printBoard.js
+++ board/printBoard.js
@@ -1,3 +1,32 @@
-export default function printBoard(board) {
+import { placeSoldier } from "./initBoard.js";
 
+export default function printBoard(board) {
+    let columnIndex = `|`
+
+    for (let i = 1; i < board.length; i++) {
+        columnIndex += ` ${i} |`
+    }
+    console.log(columnIndex);
+
+    for (const row of board) {
+        let rowToPrint = `|`
+
+        for (let i = 0; i < board.length; i++) {
+        
+            if (row[i] === 0) {
+                rowToPrint += ` ${row[i]} |`
+            } else if (row[i].owner === 'computer') {
+                rowToPrint += ' X |' 
+            } else {
+                rowToPrint += ` ${row[i].rank} |`
+            }
+        }
+        console.log(rowToPrint);
+    }
 }
+
+const board = placeSoldier()
+
+// console.log(board);
+
+printBoard(board)
